fe0cd801847a12459f9c2f92e496e3730adbd962
Alireza Chahardoli <140661782+AlirezaChahardoli@users.noreply.github.com>
2025-07-28 21:13:38 +0330

    Update Train_Test_loop.py

--- Train_Test_loop.py
+++ Train_Test_loop.py
@@ -43,3 +43,38 @@ def train_loop(model=nn.Module,
     print(f"\033[92m Learinig rate:{current_lr:.6f}\nTrain_loss:{train_loss:.4f}%\n Train_acc:{train_acc:.4f}%\nDevice is on {device}\nTime passed for training:{end_time_train-start_time_train:.2f}s\033[0m")
 
     return train_acc,train_loss
+
+
+
+def test_loop(model=nn.Module,
+               data=torch.utils.data.DataLoader,
+               loss_fn=nn.Module,
+               accuracy_fn=torchmetrics,
+               scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau,
+               device=torch.device):
+    torch.cuda.manual_seed(42)
+    start_time_test=timer()
+    test_acc,test_loss=0,0
+    model.eval().to(device)
+    with torch.inference_mode():
+        for image,label in data:
+            image,label=image.to(device),label.to(device)
+            y_pred_prob=model(image)
+            loss=loss_fn(y_pred_prob,label)
+            test_loss+=loss
+            acc=accuracy_fn(label,y_pred_prob.argmax(dim=1))
+            test_acc+=acc
+            
+
+        test_acc/=len(data)
+        test_loss/=len(data)
+        
+        end_time_test=timer()
+
+
+        print(f"\033[94mTest_loss:{test_loss:.4f}%\n Test_acc:{test_acc:.4f}%\nDevice is on {device}\nTime passed for testing:{end_time_test-start_time_test:.2f}s\033[0m")
+        scheduler.step(test_loss)
+        current_lr=scheduler.optimizer.param_groups[0]['lr']
+        print(f'\033[94m Learning Rate after scheduler step:{current_lr:.6f}')
+
+        return test_acc,test_loss
